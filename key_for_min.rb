19068b1e638eb8cc0f40d21f7a5b8d90c6852b8a
Billie Simmons <billiesnyc@gmail.com>
2018-08-18 10:22:13 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,10 +6,11 @@ def key_for_min_value(name_hash)
     puts "Hey! No hash was given!"
     
   else
+    
     min_value_key = []
     
     name_hash.each do |key, value|
-        if min_value_key[0] > value
+        if min_value_key[0] < value
         min_value_key.shift(key)
       else 
       end
