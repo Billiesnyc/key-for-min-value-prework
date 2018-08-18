fd4a2e9077690a0a32f2d21ee40877b2aaabe03b
Billie Simmons <billiesnyc@gmail.com>
2018-08-18 10:17:13 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
     min_value_key = []
     
     name_hash.each do |key, value|
-        if min_value_key[0] > key
+        if min_value_key[0] > value
         min_value_key.shift(key)
     end
     min_value_key[0]
